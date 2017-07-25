ad30f5a437eff75ecc489f39cbc3c9a7d94d5fd7
qiansen <boss.yuan@outlook.com>
2017-07-25 20:08:56 +0800

    创建表单

---
+++ views/form.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from flask_login import current_user
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length, ValidationError, Regexp, EqualTo
+from models import User
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from custom import CancelField
+
+
+class LoginForm(FlaskForm):
+	username = StringField('用户名', validators=[DataRequired(message='用户名不能为空'), Length(1, 64)])
+	password = PasswordField('密码', validators=[DataRequired(message='请输入密码')])
+	remember_me = BooleanField('记住我')
+	submit = SubmitField('登录')
+	
+	def validate_username(self, field):
+		if not User.query.filter_by(username=field.data):
+			raise ValidationError('用户名不存在')
+
+
+class ResetPasswordForm(FlaskForm):
+	username = StringField('用户名', validators=[
+		DataRequired(message='用户名不能为空'), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, '用户名只能由字母，数字，小数点和下划线组成')])
+	old_password = PasswordField('旧密码', validators=[DataRequired(message='密码不能为空')])
+	password = PasswordField('新密码', validators=[DataRequired(message='密码不能为空'), EqualTo('password2', message='密码不一致')])
+	password2 = PasswordField('确认密码', validators=[DataRequired()])
+	submit = SubmitField('确认修改')
+	cancel = CancelField('取消')
+	
+	def validate_old_password(self, field):
+		if not current_user.verify_password(field.data):
+			raise ValidationError('密码不正确')
+		
+	# def validate_username(self, field):
+	# 	if User.query.filter_by(username=field.data).first():
+	# 		raise ValidationError('用户名已存在')
