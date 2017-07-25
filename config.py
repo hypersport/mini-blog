37e231e4d4950ac908714c807b4fc4e124cb4aeb
qiansen <boss.yuan@outlook.com>
2017-07-25 20:07:31 +0800

    配置文件

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+import sys
+
+
+class Config():
+	SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'danxinghaoshimowenqiancheng')
+	SSL_DISABLE = False
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	SQLALCHEMY_RECORD_QUERIES = True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	FLASKY_SLOW_DB_QUERY_TIME = 0.5
+	
+	@staticmethod
+	def init__app(app):
+		pass
+
+
+class DevelopmentConfig(Config):
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/flask_dev'
+
+
+class ProductionConfig(Config):
+	DEBUG = False
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/flask_pro'
+
+
+config = {
+	'default': ProductionConfig,
+	'development': DevelopmentConfig,
+	'production': ProductionConfig
+}
